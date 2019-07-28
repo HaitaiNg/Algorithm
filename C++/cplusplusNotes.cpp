2dfa8a91d5c0a6fc50ab701d9546f70d54b82122
hkng248 <hkng248@gmail.com>
2019-07-27 20:52:32 -0400

    test

--- C++/cplusplusNotes.cpp
+++ C++/cplusplusNotes.cpp
@@ -1,20 +1,3 @@
-
- //stringstream
- #include <iostream>
- #include <string>
- #include <sstream>
-
- using namespace std;
- int main()
- {
-  string myStr;
-  int myInt;
-  float myFloat:
-  std::cin >> myStr >> myInt >> myFloat;
-  stringstream(myStr) >> myInt;
- }
-
-
  // arguments passed by value and reference
   #include <iostream>
  using namespace std;
@@ -30,64 +13,16 @@
  {
  int a = 10, b = 2, c = 30;
  duplicate(a, b c); // a = 20, b = 10, c = 300
- cout << a << b << c; // a = 20, b = 10, c = 300
  return 0;
  }
-
- Arguments by reference do not require a copy. The function
- operates directly on (aliases of) the strings passed
- as arguments and, at most, it might mean the transfer of
- certain pointers to the function
- On the flip side, functions with reference parameters are
- generally perceived as functions that modify arguments passed
- because that is why reference parameters are actually for.
-
- A :: string concatenate (string a, string b) {return a + b;}
- B :: string concatenate (string& a, string& b) {return a+b;}
- C :: string concatenate (const string& a, const string& b) {return a+b;}
-
- B will modify the arguments A and B
- C will not modify the reference parameters in the function, By qualifying
- them as const, the function is forbidden to modify the values of netiher a
- nor b, but can actually access their values as references (aliases of the
- arguments), without having to make actual copies of the strings.
- const references provide functionality similar to passing arguments by
- value, but with an increased efficiency for parameters of
- large types.
-
- #factorial
- #include<iostream>
- using namespace std;
-
- long factorial(int n)
- {
-  if(n >= 1)
-  {
-  return a * factorial(n-1)
-  }
-  else
-  {
-  return 1;
-  }
- }
-
-int main()
-{
-  factorial(5);
-}
-
-int foo[5] = {1,2,3,4,5};
-int baz[5] = {};
-int ccc = {10,20,30 }; // compiler will convert this to : {10,20,30,0,0};
-int foo [] = {1,2,3,4,5}; //compiler will automatically determine the size for you
-sexier way of creating arrays:
-int foo {10,20,30,40};
 
 In C++, it is syntactically correct to exceed the valid range of indices for an array.
 This can create problems, since accessing out-of-range elements do not cause errors on compilation,
  but can cause errors on runtime. The reason for this being allowed will be seen in a later chapter when pointers are introduced.
 
-At some point, we may need to pass an array to a function as a parameter. In C++, it is not possible to pass the entire block of memory represented by an array to a function directly as an argument. But what can be passed instead is its address. In practice, this has almost the same effect, and it is a much faster and more efficient operation.
+At some point, we may need to pass an array to a function as a parameter.
+In C++, it is not possible to pass the entire block of memory represented by an array to a function directly as an argument.
+But what can be passed instead is its address. In practice, this has almost the same effect, and it is a much faster and more efficient operation.
 
 To accept an array as parameter for a function, the parameters can be declared as the array type, but with empty brackets, omitting the actual size of the array. For example:
 void procedure (int arg[])
@@ -101,23 +36,23 @@ Doubly Linked List to the next node / previous node
 head = beginning of the linkedd List (only 1 pointer to the next node)
 tail = final node (only 1 pointer to the previous node)
 
-Stacks and Queues: the whole organization is based on when the elements were placed in that structure (abstract data structure)
 Stacks : LIFO
 Queues: FIFO
-Implementation Independent data structures: stack, tree, queue, sorted list, graph
 
 Any command starting with a # in the first column is not a C++ / C statement
-rather it is a preprocessor statement. The preprocessor performed very basic text-based or lexical substitutuions
+rather it is a preprocessor statement.
+The preprocessor performed very basic text-based or lexical substitutuions
 Output is sent tothe compiler
 Sequence is : file (filename.cpp) -> preprocessor -> compiler (g++)
 
-Templates: Abstract way to implement algorithms: increases the reusability (one of the main reasons of OOP);
+Templates: Abstract way to implement algorithms:
+increases the reusability (one of the main reasons of OOP);
 Templates can be used with any data structures
 
 Selection Sort
--Scan the array to find the smallest element and swap it with the first element
--Starting with the second element, scan the elements to the right of it and find the smallest
-and swap it with the second element
+Scan the array to find the smallest element and swap it with the first element
+Starting with the second element, scan the elements to the right of it and
+find the smallest and swap it with the second element
 
 Pseudo code: O(n^2) on all inputs, O(n) swaps
 for i <- 0 to n -2 do
@@ -232,34 +167,6 @@ In-order: left -> root -> right
 Pre-order: root -> left -> right
 Post-order: left -> right -> root
 
-Longest Substring without repeating characters
-
-
-
-#include <vector>
-#include <iterator>
-#include <algorithm>
-
-using namespace std;
-
-
-class Solution {
-public:
-    int smallestRepunitDivByK(int K) {
-        if(K % 2 == 0 || K % 5 == 0)
-        {
-            return -1;
-        }
-        int r = 0;
-        for(int i = 1; i <= K; i++)
-        {
-            int r = (r * 10 + 1) % K;
-            if( r == 0){return i;}
-        }
-        return -1;
-    }
-};
-
 Double-ended queues are sequence containers
 with dynamic sizes that can be expanded or contracted
 on both ends (either its front or its back)
@@ -267,3 +174,60 @@ Deques are not guaranteed to store all of its
 elements in contiguous storage locations:
 accessing elements in a deque by offsetting a
 pointer to another element causes undefined behavior
+
+A pointer in C is a way to share a memory address among
+different contexts (prmarily functions). They
+are primarily used whenever a function needs to modify the content
+of a variable, of which it doesn't have ownership.
+& returns the memory address of a variable
+int*p = &val; //asigns the memory address of val to pointer p
+To access the content of the memory to which the pointer points,
+prepend it with a *. *p will return the value reflected by val
+and any modications to it will be reflected at the source
+
+
+void increment(int *v)
+{
+  (*v)++;
+}
+
+int main()
+{
+  int a;
+  scanf("%d", &a);
+  increment(&a);
+  printf("%d", a);
+  return 0;
+}
+
+-----------------
+
+#include <stdio.h>
+#include <cmath>
+
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int absDifference = *a - *b > 0 ? *a - *b : -(*a - *b);
+    *a = sum;
+    *b = absDifference;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+    return 0;
+}
+------------
+
+int main() {
+    int a; long b; char c; float d; double e;
+    scanf("%i %li %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%i\n%li\n%c\n%.03f\n%.09lf\n", a, b, c, d, e);
+    return 0;
+}
+An array is a series of elements of the same type placed in contiguous memory locations that can be individually referenced by adding an index to a unique identifier.
+
+stringstream is used to parse-common stringstream
