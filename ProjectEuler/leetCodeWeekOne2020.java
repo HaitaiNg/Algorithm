e36d155574e7824dadf150fb7e907d7359118124
haitai <hkng248@gmail.com>
2020-04-03 09:02:49 -0400

    Day Three

--- ProjectEuler/leetCodeWeekOne2020.java
+++ ProjectEuler/leetCodeWeekOne2020.java
@@ -32,7 +32,17 @@ public class leetCodeWeekOne2020{
         return isHappyNumberHelper(n, numberSet); 
     }
 
-    //4.3.20
+    //4.3.20 Maximum Sub Array (Kadane's Algorithm) 
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 0) return Integer.MIN_VALUE; 
+        int currentWindowSum = nums[0]; 
+        int largestSum = nums[0]; 
+        for(int i = 1; i < nums.length; i++){
+            currentWindowSum = Math.max(currentWindowSum + nums[i], nums[i]); 
+            largestSum = Math.max(currentWindowSum, largestSum); 
+        }
+        return largestSum; 
+    }
     //4.4.20
     //4.5.20
     //4.6.20
@@ -45,5 +55,8 @@ public class leetCodeWeekOne2020{
         object.singleNumber(new int[] {4,1,2,1,2}); 
         //4.2.20 // < Solution = true 
         object.isHappy(19); 
+        //4.4.20 // < Solution = 6 (input =[-2,1,-3,4,-1,2,1,-5,4]) : [4,-1,2,1] has the largest sum = 6.
+        object.maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,-4}); 
+
     }
 }
