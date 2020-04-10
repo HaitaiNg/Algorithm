846bccb87a31762cafd0c24fb887912beb8cffbc
haitai <hkng248@gmail.com>
2020-04-09 21:32:04 -0400

    test

--- ProjectEuler/projectEulerThree.java
+++ ProjectEuler/projectEulerThree.java
@@ -7,8 +7,20 @@ public class projectEulerThree{
         ///< XXXI Coin Sums (31) : Dynamioc Progrmaming 
         public int coinSums()
         {
-                return 0 ;
+                int target = 200; 
+                int[] coinSizes = {1,2,5,10,20,50,100,200}; 
+                int[] ways = new int[target+1]; 
+                ways[0] = 1; 
+                for(int i = 0; i < coinSizes.length; i++)
+                {
+                        for(int j = coinSizes[i]; j <= target; j++)
+                        {
+                                ways[j] += ways[j - coinSizes[i]]; 
+                        }
+                }
+                return ways[200];
         }
+
         //< XXXII Pandigital Products (32) 
         public int pandigitalProducts()
         {
@@ -89,10 +101,53 @@ public class projectEulerThree{
                 return returnValue; 
         }
 
+        /// XXXV Circular Primes (35) :: Incomplete 
+        public int circularPrimes()
+        {
+                int numberOfCircularPrimes = 0; 
+                for(int i = 0 ; i < 1000000; i++)
+                {
+                        if(checkCircular(i)){
+                                numberOfCircularPrimes++; 
+                        }
+                }
+                System.out.println(numberOfCircularPrimes);
+                return numberOfCircularPrimes; 
+        }
+        public boolean checkCircular(int n){
+                int count = 0, temp = n; 
+                while(temp > 0){
+                        count++; 
+                        temp /= 10; 
+                }
+                int num = n; 
+                while(isPrime(n))
+                {
+                        int rem = num % 10;
+                        int div = num / 10; 
+                        num = (int) ((Math.pow(10, count - 1)) * rem) + div; 
+                        if(num == n); return true; 
+                }
+                return false; 
+        }
+        public boolean isPrime(int n )
+        {
+                if(n <= 1) return false;
+                if(n <= 3) return true; 
+                if( n % 2 == 0 || n % 3 == 0) return false; 
+                for(int i = 5; i * i <= n; i = i + 6) 
+                {
+                        if(n % i == 0 || n % ( i + 2) == 0) return false; 
+                }
+                return true; 
+        }
+
 	public static void main(String[] args){
         projectEulerThree project = new projectEulerThree(); 
-        project.pandigitalProducts(); //< Solution : 45228 (32) 
-        project.digitFactorials(); //< Solution: 40730 (34) 
+        // project.coinSums(); //< Solution: 73682 (31)
+        // project.pandigitalProducts(); //< Solution : 45228 (32) 
+        // project.digitFactorials(); //< Solution: 40730 (34) 
+        project.circularPrimes(); //< Solution : 55 (35) 
 
 	} 
 }
