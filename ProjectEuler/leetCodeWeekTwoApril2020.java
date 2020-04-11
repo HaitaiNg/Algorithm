59ee70f584194811788978f6e10c40fc8d299a0e
haitai <hkng248@gmail.com>
2020-04-11 18:58:46 -0400

    test

--- ProjectEuler/leetCodeWeekTwoApril2020.java
+++ ProjectEuler/leetCodeWeekTwoApril2020.java
@@ -92,6 +92,33 @@ public class leetCodeWeekTwoApril2020{
         }
    }
 
+
+
+   // 4.11 Diameter of a binary tree 
+   
+   public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+        }
+        int ans; 
+   
+   public int diameterOfBinaryTree(TreeNode root) {
+       ans = 0;
+       ans = 1; 
+       depth(root);
+       return ans - 1; 
+   }
+       public int depth(TreeNode node) {
+       if (node == null) return 0;
+       int L = depth(node.left);
+       int R = depth(node.right);
+       ans = Math.max(ans, L+R+1);
+       return Math.max(L, R) + 1;
+   }
+
+
     public static void main(String[] args){
         leetCodeWeekTwoApril2020 object = new leetCodeWeekTwoApril2020(); 
         System.out.println(object.backspaceCompare("ab#c", "ad#c")); //< 4.9 
