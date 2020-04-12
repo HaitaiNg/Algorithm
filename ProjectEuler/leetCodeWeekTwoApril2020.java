461212550e1ccd33e02881c4e69cd779fd664b19
haitai <hkng248@gmail.com>
2020-04-12 09:45:53 -0400

    4.12

--- ProjectEuler/leetCodeWeekTwoApril2020.java
+++ ProjectEuler/leetCodeWeekTwoApril2020.java
@@ -95,7 +95,6 @@ public class leetCodeWeekTwoApril2020{
 
 
    // 4.11 Diameter of a binary tree 
-   
    public class TreeNode {
          int val;
          TreeNode left;
@@ -119,9 +118,65 @@ public class leetCodeWeekTwoApril2020{
    }
 
 
+   // 4.12 Last Stone Weight 
+   public int lastStoneWeight(int[] stones) {
+       Arrays.sort(stones); 
+       if(stones.length == 0) return 0; //< base case 
+       if(stones.length == 1) return stones[0]; //< base case 
+      
+       int rightPointer = stones[stones.length - 1]; 
+       int leftPointer = stones[stones.length - 2];
+       int difference = rightPointer - leftPointer;
+
+       stones = removeTheElement(stones, rightPointer);
+       stones = removeTheElement(stones, leftPointer); 
+       if(difference != 0)
+       {
+            stones = addElement(stones.length, stones, difference); 
+       }
+       return lastStoneWeight(stones);
+   }   
+
+   public static int[] removeTheElement(int[] arr, 
+                                          int index ) 
+    { 
+        // Create another array of size one less 
+        int[] anotherArray = new int[arr.length - 1]; 
+        // Copy the elements except the index 
+        // from original array to the other array 
+        for (int i = 0, k = 0; i < arr.length - 1; i++) { 
+            anotherArray[k++] = arr[i]; 
+        } 
+        // return the resultant array 
+        return anotherArray; 
+    } 
+      // Function to add x in arr 
+      public int[] addElement(int n, int arr[], int x) 
+      { 
+          int i; 
+    
+          // create a new array of size n+1 
+          int newarr[] = new int[n + 1]; 
+    
+          // insert the elements from 
+          // the old array into the new array 
+          // insert all elements till n 
+          // then insert x at n+1 
+          for (i = 0; i < n; i++) 
+              newarr[i] = arr[i]; 
+    
+          newarr[n] = x; 
+    
+          return newarr; 
+      } 
+
+
+
+
     public static void main(String[] args){
         leetCodeWeekTwoApril2020 object = new leetCodeWeekTwoApril2020(); 
-        System.out.println(object.backspaceCompare("ab#c", "ad#c")); //< 4.9 
+        // object.backspaceCompare("ab#c", "ad#c"); //< 4.9
+        // object.lastStoneWeight(new int[] {2,7,4,1,8,1}); //< 4.12 Solution : 1 
    }
 
 }
