0932c9d7825cb4484d846fa6118c66b0d3af451f
haitai <hkng248@gmail.com>
2020-04-14 18:27:43 -0400

    leetcode4.14

--- ProjectEuler/leetCodeWeekTwoApril2020.java
+++ ProjectEuler/leetCodeWeekTwoApril2020.java
@@ -188,11 +188,85 @@ public class leetCodeWeekTwoApril2020{
         return maxlen; 
     }
 
+        public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+            Collections.sort(array);
+            int leftPointer = array.indexOf(toMove); 
+            int rightPointer = array.size() - 1;
+            while(leftPointer <= rightPointer)
+            {
+               int leftValue = array.get(leftPointer); 
+               int rightValue = array.get(rightPointer); 
+               if(leftValue == toMove)
+               {
+                   array.set(leftPointer, rightValue); 
+                   array.set(rightPointer, toMove); 
+                   rightPointer--; 
+                }
+                leftPointer++; 
+            }
+            return array; 
+      }
+    
+   // 4.14 Perform String Shifts (1 hour problem)
+   // I thought I could use a hashmap to store the characters and index but I forgot that there could be repeats in the string
+   // Difficulty understanding how character arrays are dereferenced from each other 
+   public String stringShift(String s, int[][] shift) {
+       char[] originalString = new char[s.length()];
+       char[] modifiedString = new char[s.length()];  
+       int shiftLeftOrRight = 0, numberOfShifts = 0; 
+       for(int i = 0; i < s.length(); i++){
+           originalString[i] =  s.charAt(i);
+           modifiedString[i] = s.charAt(i); 
+       } 
+        for(int i = 0; i < shift.length; i++)
+        {
+            shiftLeftOrRight = shift[i][0]; 
+            numberOfShifts = shift[i][1]; 
+            for(int j = 0; j < originalString.length; j++)
+            {
+                int index = j; 
+                char currentCharacter = originalString[j]; 
+                if(shiftLeftOrRight == 1)
+                {
+                    index = index + numberOfShifts; 
+                    if(index >= s.length()) index = index % s.length(); 
+                }
+                else if(shiftLeftOrRight == 0)
+                {
+                    if(index - numberOfShifts < 0){
+                        index = (s.length() + index) - numberOfShifts; 
+                    }
+                    else
+                    {
+                        index = index - numberOfShifts;
+                    }
+                }
+               
+                modifiedString[index] = currentCharacter; 
+                System.out.println(String.valueOf(modifiedString) + "  " + currentCharacter + " " + String.valueOf(originalString));
+            }
+            for(int z = 0; z < modifiedString.length; z++){
+                originalString[z] =  modifiedString[z];
+            } 
+           
+                /// hello
+                /// 01234
+                /// 0, 3 
+                /// h :: 0 - 3 = -3 ::  2 // s.length - 3 = 5 -3 = 2 
+                /// e :: 1 - 3 = -2 :: 3  // (s.length + 1) - 3 =  6 - 3 = 3  
+                /// l :: 2 - 3 = -1 :: 4  / (s.length + 2 ) - 3 =  4 
+                /// l :: 3 - 3 = 0 
+                /// o :: 4 - 3 = 1 
+                /// lohel
+        }
+    return String.valueOf(originalString);;
+}
 
     public static void main(String[] args){
         leetCodeWeekTwoApril2020 object = new leetCodeWeekTwoApril2020(); 
         // object.backspaceCompare("ab#c", "ad#c"); //< 4.9
         // object.lastStoneWeight(new int[] {2,7,4,1,8,1}); //< 4.12 Solution : 1 
+        // object.stringShift("abc", new int[][] {{0,1}, {1,2}}); //< 4.15 Solution : cab 
    }
 
 }
