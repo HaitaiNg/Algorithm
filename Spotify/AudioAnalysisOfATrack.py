19dde29de3aee242e1be193055500382ef84f8e2
haitai <hkng248@gmail.com>
2020-06-09 10:58:50 -0400

    test

--- Spotify/AudioAnalysisOfATrack.py
+++ Spotify/AudioAnalysisOfATrack.py
@@ -2,7 +2,37 @@ import sys
 import spotipy
 import spotipy.util as util
 
+#Get the list of the top trakcs for a user 
+def getUserTopTracks(sp):
+    for range in ranges:
+        print("range:", range.rstrip())
+        results = sp.current_user_top_tracks(time_range=range, limit=10)
+        for i, item in enumerate(results['items']):
+            artist, songName, spotifyURL = iterateThroughListContents(item['artists']), item["name"], item["external_urls"]["spotify"]
+            print(songName + " :: " + artist + " :: " + spotifyURL)
+        print("\n") 
 
+#Get the list of the top artists for a user 
+def getUserTopArtists(sp):
+    genreTypesAndFrequency = {} 
+    for range in ranges:
+        print("range:", range)
+        results = sp.current_user_top_artists(time_range=range, limit=20)
+        for i, item in enumerate(results['items']):
+            artist, genres, spotifyURL = item['name'],  [ f.encode('ascii', 'ignore') for f in item['genres']], item['external_urls']['spotify']
+            print(artist + " :: " + str(genres))
+            for genre in genres: 
+                if genre in genreTypesAndFrequency.keys():
+                    genreTypesAndFrequency[genre] +=1 
+                else:
+                    genreTypesAndFrequency[genre] = 1 
+        print("\n")      
+
+    for k in sorted(genreTypesAndFrequency.keys()):
+        print(k + " " + str(genreTypesAndFrequency[k]))
+    print(len(genreTypesAndFrequency.keys())) 
+
+#Iterate through a dictionary and extract the list contents to create a string 
 def iterateThroughListContents( listInput):
     if(len(listInput) > 0):
         listOfContents = []  
@@ -13,7 +43,7 @@ def iterateThroughListContents( listInput):
     return str(listInput[0])
 
 
-
+#MAIN 
 if len(sys.argv) > 1:
     username = sys.argv[1]
 else:
@@ -27,39 +57,9 @@ if token:
     sp = spotipy.Spotify(auth=token)
     sp.trace = False
     ranges = ['short_term', 'medium_term', 'long_term']
-
-    ''' 
     #Get the list of the top tracks for a user 
-    for range in ranges:
-        print("range:", range)
-        results = sp.current_user_top_tracks(time_range=range, limit=50)
-        for i, item in enumerate(results['items']):
-            artist, songName, spotifyURL = iterateThroughListContents(item['artists']), item["name"], item["external_urls"]["spotify"]
-            print(songName + " :: " + artist + " :: " + spotifyURL)
-        print("\n") '''
-        
-    genreTypesAndFrequency = {} 
-    
+    getUserTopTracks(sp) 
     #Get the list of the top artists for a user 
-    
-    for range in ranges:
-        print("range:", range)
-        results = sp.current_user_top_artists(time_range=range, limit=50)
-        for i, item in enumerate(results['items']):
-            artist, genres, spotifyURL = item['name'],  [ f.encode('ascii', 'ignore') for f in item['genres']], item['external_urls']['spotify']
-            print(artist + " :: " + str(genres))
-
-            for genre in genres: 
-                if genre in genreTypesAndFrequency.keys():
-                    genreTypesAndFrequency[genre] +=1 
-                else:
-                    genreTypesAndFrequency[genre] = 1 
-
-        print("\n")  
-    print("\n")
-    for k in sorted(genreTypesAndFrequency.keys()):
-        print(k + " " + str(genreTypesAndFrequency[k]))
-
-
+    getUserTopArtists(sp)
 else:
     print("Can't get token for", username)
---
+++ Spotify/README.md
@@ -0,0 +1,3 @@
+Log: 
+06.07.2020 : Found and experiment with Spotify APIs. Continuing to research on viable APIs used to achieve goal 
+06.08.20 : Introduction to Flask (Connecting the python backend with the front end) 
