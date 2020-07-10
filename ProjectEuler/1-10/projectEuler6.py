12c3bb9c172cc4ed428143a71cb408b00b26722c
haitai <hkng248@gmail.com>
2020-07-09 22:53:57 -0400

    project euler 7

--- ProjectEuler/1-10/projectEuler6.py
+++ ProjectEuler/1-10/projectEuler6.py
@@ -1,11 +1,8 @@
 """
 Description: Find the absolute difference between the sum of the squares of the
-first natural numbers and the square of the sum. 
-
-Used the mathematical sigma notation for sum of squares of n natural numbers:
-n(n*1)(2n+1)/6 
-
-Completed using Python 3 
+first natural numbers and the square of the sum. I used the 
+mathematical sigma notation for sum of squares of n natural numbers:
+n(n*1)(2n+1)/6. Completed using Python 3 
 """
 
 #!/bin/python3
---
+++ ProjectEuler/1-10/projectEuler7.py
@@ -0,0 +1,33 @@
+""" Description: Calculate the Nth prime number. Completed using 
+Python 3.6.5. 
+
+This problem is very tricky since the solution
+has to be optimized and efficient. I faced a lot of run time 
+errors / time limit exceeded. I created a modified version of 
+sieve of eratosthenes however this would result in a time limit
+exceeded. This is a problem I will have to re-attempt again """ 
+
+#!/bin/python3
+import math
+
+def prime(n,listOfPrimeNumbers):
+    x=listOfPrimeNumbers[-1]
+    while len(listOfPrimeNumbers) < n:
+        x+=2
+        y=math.floor(x**0.5)
+        for i in listOfPrimeNumbers:
+            if i>y:
+                listOfPrimeNumbers.append(x)
+                break
+            if x%i==0:
+                break
+    return listOfPrimeNumbers
+
+
+t=int(input())
+listOfPrimeNumbers =[2,3]
+for i in range(t):
+    n=int(input())
+    if len(listOfPrimeNumbers)<n:
+        listOfPrimeNumbers =prime(n,listOfPrimeNumbers)
+    print(listOfPrimeNumbers[n-1])
