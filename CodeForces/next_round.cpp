5497cb84aada6228c2de81ceb4dccd49b73d2407
haitai <hkng248@gmail.com>
2020-10-05 22:24:26 -0400

    Why is troubleshooting C++ so difficult

---
+++ CodeForces/next_round.cpp
@@ -0,0 +1,24 @@
+// CodeForces
+// Theatre Square 
+// GNU C++ 17 
+
+
+#include <iostream>
+#include <sstream> 
+#include <string> 
+#include <vector> 
+using namespace std; 
+
+int main()
+{
+    long long n = 0, k = 0, studentsToNextRound = 0; 
+    cin >> n >> k;
+    cin.ignore(); 
+    string s; 
+    getline(cin, s); 
+
+    // Parse the list of scores
+    // Find the score of the kth person 
+    // Compare all answers to that kth person's score
+    // If score >= kth's score, that student advances to next round 
+}
