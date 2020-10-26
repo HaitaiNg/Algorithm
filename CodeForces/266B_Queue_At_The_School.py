f02b330a6b95336b8824055c36b82c4d12628856
haitai <hkng248@gmail.com>
2020-10-25 20:03:47 -0400

    CF

--- CodeForces/266B_Queue_At_The_School.py
+++ CodeForces/266B_Queue_At_The_School.py
@@ -1,11 +1,40 @@
 #CodeForces
-#Queue at the school
-#Python 3.8.5 
+#Queue At the School
+#Python 3.6.5
 
 import sys
 
-school_info = input()
-school_info = school_info.split() 
-n, t = school_info[0], scool_info[1]
-s = input()
+def studentSort(students, time, start):
 
+    for i in range(start, len(students), 2):
+        if(students[i] == "G" and students[i-1] == "B"):
+            students[i-1], students[i] = "G","B"
+            #print(students)
+        else:
+            pass
+    if(len(students) % 2 == 1):
+        if(students[-1] == "G" and students[-2] == "B"):
+            students[-2], students[-1] = "G","B"
+    time = time - 1
+
+    if(start == 1):
+        start = 0
+    else:
+        start = 1 
+    
+    if(time > 0):
+        students = studentSort(students,time, start)
+
+    if(start == 1):
+        students.reverse()
+    return students
+        
+
+
+
+
+
+studentCount_time = input().split()
+time = int(studentCount_time[1]) 
+students = list(input())
+print(''.join(studentSort(students, time, 1)))
