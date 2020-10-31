5a91195c6aaa963446bc352c98c36b4a297ed8e4
haitai <hkng248@gmail.com>
2020-10-31 15:58:59 -0400

    C

---
+++ CodeForces/Contest_Questions/MarketingScheme.py
@@ -0,0 +1,11 @@
+#CodeForces
+#Marketing Scheme
+ 
+for _ in range(int(input())):
+    a,b = map(int, input().split())
+    print(a)
+    print(b)
+    if b >= 2*a:
+        print("NO")
+    else:
+        print("YES") 
