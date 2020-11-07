c21f48009c093fc2cb41c17404ad5d3d7c7bb9b5
haitai <hkng248@gmail.com>
2020-11-06 22:05:03 -0500

    RM CF

--- CodeForces/1335A_Candies_And_Two_Sisters.py
+++
@@ -1,9 +0,0 @@
-#CodeForces
-#Candies And Two Sisters
-
-for i in range(int(input())):
-    j = int(input())
-    if( j % 2 == 0):
-        print( j // 2 - 1)
-    else:
-        print( j // 2) 
--- CodeForces/281A_Word_Capitalization.py
+++
@@ -1,8 +0,0 @@
-#CodeForces 
-#Word Capitalization  
-#Python 3.6.5 
-
-import sys 
-
-capitalize = input()
-print(capitalize[0].upper()+capitalize[1:])
