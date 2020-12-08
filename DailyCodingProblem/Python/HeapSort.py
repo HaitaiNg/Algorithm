16d08d681a2b35d9a6ecd282a7327a43d9d21112
haitai <hkng248@gmail.com>
2020-12-07 22:39:01 -0500

    LC

--- DailyCodingProblem/Python/HeapSort.py
+++ DailyCodingProblem/Python/HeapSort.py
@@ -33,3 +33,4 @@ def siftDown(currentIdx, endIdx, heap):
 
 def swap(i, j, array):
     array[i], array[j] = array[j], array[i]
+ 
---
+++ DailyCodingProblem/Python/quicksort_iv.py
@@ -0,0 +1,20 @@
+from random import randint 
+def quicksort(array):
+    if len(array) < 2: 
+        return array 
+    low, same, high = [], [], [] 
+
+    pivot = array[randint(0, len(array) - 1)]
+    for item in array:
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+
+    return quicksort(low) + same + quicksort(high) 
+
+
+array = [29,99,27,41,66,28,44,78,87,19,31,76,58,88,83,97,12,21,44]
+print(quicksort(array))
