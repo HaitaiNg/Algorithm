3721b8b69b6690f6b49340e414db22297c39cebb
haitai <hkng248@gmail.com>
2020-12-28 22:01:11 -0500

    Coursera

---
+++ Coursera/Algorithms_I/README.md
@@ -0,0 +1,10 @@
+# Algorithm I 
+###### Refresh Date: 12.28.20
+###### Author: Haitai Ng 
+
+My notes from Princeston University Algorithms I course on Coursera.
+All code samples were provided in the lecture notes. This course is free on Coursera. 
+
+
+
+
---
+++ Coursera/Algorithms_I/linked_list_iterator.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item>{
+    public Iterator<Item> iterator(){
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item>{
+        private Node current = first ;
+        public boolean hasNext() { return current != null;}
+        public void remove() {}
+        public Item next(){
+            Item item = current.item;
+            current = current.next;
+            return item; 
+        }
+    }
+}
